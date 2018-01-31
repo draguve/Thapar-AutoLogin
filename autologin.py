85becaa55078dbb6b4febc886a0578f92c733a69
Draguve <ritwikpatil1998@gmail.com>
2018-02-01 01:15:03 +0530

    Added SIGINT to logout

--- autologin.py
+++ autologin.py
@@ -1,5 +1,17 @@
 import requestmanager
 import argparse
+import signal
+
+username = "username"
+password = "password"
+
+#function called when ctrl_c is called
+def ctrl_c(signal, frame):
+    requestmanager.logout_user(username)
+    print("Logging out " + username)
+    exit()
+
+signal.signal(signal.SIGINT, ctrl_c)
 
 if __name__ == "__main__":
-    requestmanager.login("roll number","password",120)
+    requestmanager.login(username,password,120)
